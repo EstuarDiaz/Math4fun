c9017f90bd63c38557d81eb4a2fdd5437921f5b8
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2017-11-06 18:37:56 -0600

    change jquery

--- examples/content.js
+++ examples/content.js
@@ -115,8 +115,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = $('#videosContainer').find('.mySlides');
+  var dots = $('#videosDots').find('.dot');
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
